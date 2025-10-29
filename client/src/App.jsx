8ef3bbf1e2214102d883bfb921ca51e73965ea7d
Schrodinger's Doggo <alammdafzal29@gmail.com>
2025-10-29 19:35:24 +0530

    Added frontend of College Predictor

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import DarkMode from './DarkMode'
+import {  CollegePredictor, StepIndicator } from './Pages/CollegePredictor'
+
+function App() {
+  return (
+    <CollegePredictor/>
+  )
+}
+
+export default App
